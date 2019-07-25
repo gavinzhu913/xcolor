c68cb51e4c8afbbb015c85c9d9e6664a3d0231a8
Max <max@xmail.vip>
2019-07-25 08:47:39 +0800

    修改.gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author_email = "max@artsoft.io",
     license = "MIT",
     name = "xcolor",
-    version = "1.8.2",
+    version = "1.8.4",
     keywords = "color print ColorPrint python3",
     url = "https://github.com/artsoftio/xcolor",
     install_requires = [],
